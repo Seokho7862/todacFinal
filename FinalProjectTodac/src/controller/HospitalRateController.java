16094c8723e5dfd7b129f6f2f5a62351d350a2a9
sy7 <5calalee@naver.com>
2019-08-06 20:08:11 +0900

    병원평가 함수 만드는중

---
+++ FinalProjectTodac/src/controller/HospitalRateController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HospitalRateController {
+	
+	
+	@RequestMapping("rateinfoform.do")
+	public void rateInfoForm() {
+		
+	}
+	
+	@RequestMapping("rateinfo.do")
+	public void getYkiho(@RequestParam(required=false) String code1, 
+				@RequestParam(required=false)String code2,
+				@RequestParam(required=false)String hname) {
+		List<String> ykiList = new YkihoGet1().getYkiho(code1, code2, hname);
+		for(String y: ykiList) {
+    		System.out.println(y+"controller");
+    	}
+		
+	}
+		
+}
