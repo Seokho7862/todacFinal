2bb6edaf11b9af4a545da6bab067ee37a9212268
sy7 <5calalee@naver.com>
2019-08-13 09:05:06 +0900

    회원 부분

--- FinalProjectTodac/src/controller/TestController.java
+++ FinalProjectTodac/src/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.View;
 
 import service.TestService;
 
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;;
