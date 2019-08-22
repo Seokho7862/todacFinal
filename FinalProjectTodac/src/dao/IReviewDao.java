06605110f0ab075a79bef8449838ab5a82525e11
namseokho <say7862@hanmail.net>
2019-08-22 19:26:47 +0900

    복구 완료

--- FinalProjectTodac/src/dao/IReviewDao.java
+++ FinalProjectTodac/src/dao/IReviewDao.java
@@ -2,12 +2,15 @@ package dao;
 
 import java.util.List;
 
+import model.Report;
 import model.Review;
 
 public interface IReviewDao {
 	public List<Review> selectOneHospitalReview(String hpid);
 	public Review selectOneReviewBoard(String rid);
 	public Review selectAroundReview(String rid);
-	//리뷰작성
 	public void reviewWrite(Review r);
+	public void ReportInsert(Report r);
+	public void updateReivewStatus(int rid);
+	
 }
